7eaf03f3dbb97501479b8d25e8ea4b28468b19f2
LUNDR <lundrm@gmail.com>
2020-07-14 09:51:34 +0200

    chart maintenance

--- app/figures.py
+++ app/figures.py
@@ -824,7 +824,7 @@ figure['layout'] = dict(
             title=dict(
                 text=x_title, font=dict(
                     size=x_title_font_size)), range=[
-                        0, 1000], ), yaxis=dict(
+                        0, 1200], ), yaxis=dict(
                             title=dict(
                                 text=y_title, font=dict(
                                     size=y_title_font_size)), range=[
